3bf99b342009aa551c3136ca023d3ee572b2f69f
FranRD98 <franrd98@outlook.com>
2025-04-02 12:39:07 +0200

    Eliminada información sensible del firebase config y resuelto con gitignore y variables ENV

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,15 +5,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCplDSMe15EkTdozHGDNYXeBkJu1YhKzKE",
-  authDomain: "fitvue-7757e.firebaseapp.com",
-  projectId: "fitvue-7757e",
-  storageBucket: "fitvue-7757e.firebasestorage.app",
-  messagingSenderId: "603467004329",
-  appId: "1:603467004329:web:647cedb63fba4019452cb8",
-  measurementId: "G-J97MSTDQYN"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
